e22416cb4dd8f47acf3f811b74da4695e36f761e
n <n>
2017-03-07 20:33:29 -0500

    Reorganized assets - A clean workspace is a successful workspace. Added shoot script an components to controllers in TutorialScene.

--- Assets/Scripts/Gameplay/ShootScript.cs
+++ Assets/Scripts/Gameplay/ShootScript.cs
@@ -2,21 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
+//[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(SteamVR_TrackedController))]
 public class ShootScript : MonoBehaviour {
-
-    AudioSource audioSource;
-    Animation recoilAnim;
-
-    [SerializeField]
-    AudioClip gunshotSFX;
+    private SteamVR_TrackedController _controller;
 
     RaycastHit hit;
     
     void Awake()
     {
-        recoilAnim = GetComponent<Animation>();
-        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -24,20 +18,28 @@ public class ShootScript : MonoBehaviour {
         Debug.DrawRay(transform.position, transform.forward * 100f, Color.red);
     }
 
-    public void Shoot()
+    private void OnEnable()
+    {
+        _controller = GetComponent<SteamVR_TrackedController>();
+        _controller.TriggerClicked += Shoot;
+    }
+
+    private void OnDisable()
+    {
+        _controller.TriggerClicked -= Shoot;
+    }
+
+
+    public void Shoot(object sender, ClickedEventArgs e)
     {
 
-        // Play SFX
-        audioSource.PlayOneShot(gunshotSFX, 0.3f);
+        //// Play SFX
+        //audioSource.PlayOneShot(gunshotSFX, 0.3f);
 
-        // Play Anim
-        if (recoilAnim.IsPlaying("Recoil")) recoilAnim.Stop();
-        recoilAnim.Play();
-
-
+        Debug.Log("Gun/Shoot()");
         if (Physics.Raycast(transform.position, transform.forward, out hit, 100f))
         {
-            Debug.Log(hit.transform.tag);
+            Debug.Log("Gun/Shoot() -  Raycasted against " + hit.transform.tag);
             if (hit.transform.tag == "Note")
             {
                 //hit.transform.GetComponent<Note>().destroyed = true;
