b40c65736b82056338b8438c727d95c95ffa18a9
Cat <adamhoang@live.com>
2017-03-14 21:41:13 -0400

    Implemented VR Input using Wacki's MIT Licensed VRInputModule. Link: https://github.com/wacki/Unity-VRInputModule Not perfectly integrated yet; has some flaws in rendering laser properly.

--- Assets/Scripts/Gameplay/ShootScript.cs
+++ Assets/Scripts/Gameplay/ShootScript.cs
@@ -12,18 +12,35 @@ public class ShootScript : MonoBehaviour {
 
     AudioSource audioSource;
 
+    private Camera UICamera;
 
-    
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
+    void Start()
+    {
+        //// Create a new camera that will be used for raycasts
+        //UICamera = new GameObject("UI Camera").AddComponent<Camera>();
+        //UICamera.clearFlags = CameraClearFlags.Nothing;
+        //UICamera.cullingMask = 0;
+        //UICamera.fieldOfView = 5;
+        //UICamera.nearClipPlane = 0.01f;
+
+        //// Find canvases in the scene and assign our custom UICamera to them
+        //Canvas[] canvases = Resources.FindObjectsOfTypeAll<Canvas>();
+        //foreach (Canvas canvas in canvases)
+        //{
+        //    canvas.worldCamera = UICamera;
+        //}
+    }
+
     void Update()
     {
-        Debug.DrawRay(transform.position + new  Vector3(0,0.085f,0), transform.forward * 100f, Color.red);
+        //Debug.DrawRay(transform.position + new Vector3(0,0.085f,0), transform.forward * 100f, Color.red);
 
-        TestRaycast();
+        //TestRaycast();
     }
 
     private void OnEnable()
@@ -43,10 +60,10 @@ public class ShootScript : MonoBehaviour {
         //// Play SFX
         audioSource.PlayOneShot(audioSource.clip, 0.3f);
 
-        Debug.Log("Gun/Shoot() - Raycast attempt");
+        Debug.Log("ShootScript/Shoot() - Raycast attempt");
         if (Physics.Raycast(transform.position + new Vector3(0, 0.085f, 0), transform.forward, out hit, 100f))
         {
-            Debug.Log("Gun/Shoot() -  Raycasted against " + hit.transform.tag);
+            Debug.Log("ShootScript/Shoot() -  Raycasted against " + hit.transform.tag);
             if (hit.transform.tag == "GJMonster")
             {
                 hit.collider.GetComponent<GJMonster>().Kill();
@@ -56,16 +73,19 @@ public class ShootScript : MonoBehaviour {
 
     public void TestRaycast()
     {
-        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        Vector3 rayDirection = transform.rotation * Vector3.forward;
+        Debug.DrawRay(transform.position, rayDirection, Color.red);
 
-        pointerData.position = Input.mousePosition;
+        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        pointerData.position = rayDirection;
 
         List<RaycastResult> results = new List<RaycastResult>();
         EventSystem.current.RaycastAll(pointerData, results);
 
         if (results.Count > 0)
         {
-            //WorldUI is my layer name
+            Debug.Log("ShootScript/TestRaycast() - Raycast Results Found " + results.Count);
+            //WorldUI is my layer name;
             if (results[0].gameObject.layer == LayerMask.NameToLayer("UI"))
             {
                 string dbg = "Root Element: {0} \n GrandChild Element: {1}";
@@ -74,6 +94,9 @@ public class ShootScript : MonoBehaviour {
                 //Debug.Log("GrandChild Element: "+results[0].gameObject.name);
                 results.Clear();
             }
+        } else
+        {
+            Debug.Log("ShootScript/TestRaycast() - No Targets Found.");
         }
     }
 }
