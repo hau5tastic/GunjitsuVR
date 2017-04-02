0a9f0fa2ca360c8616b8e1484a89f6c83b711889
Nicco Jacinto <nix_jacinto25@hotmail.com>
2017-04-02 00:48:50 -0400

    Add proto microchip ps

---
+++ Assets/Refactoring/MicrochipPS.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrochipPS : MonoBehaviour {
+
+    public GameObject microParticle;
+    public float interval;
+    public float amount;
+    public float lifetime;
+
+    void Start() {
+        StartCoroutine(Emit());
+    }
+
+    IEnumerator Emit() {
+        while (true) {
+            for (int i = 0; i < amount; ++i) {
+                Destroy(Instantiate(microParticle, transform.position, Quaternion.identity), lifetime);
+            }
+            yield return new WaitForSeconds(interval);
+        }
+
+    }
+}
